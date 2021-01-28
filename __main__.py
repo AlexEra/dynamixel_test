fdfddc4edf213b4d2bd9f80a79d14ee0c8be1632
Alex <erashov.ru@mail.ru>
2021-01-28 12:43:32 +0300

    v0

---
+++ __main__.py
@@ -0,0 +1,33 @@
+from dynamixel import *
+from serialsupport import *
+
+
+if __name__ == '__main__':
+    while True:
+        print("available ports: ", *serial_ports())
+        port = input("input dynamixel port, or print <<e>> for shutdown: ")
+        if port == 'e':
+            print("quit")
+            quit()
+
+        addresses = [64, 116, 132]
+        dxl = Dynamixel(addresses, 1, 1, port, 100, 4000, 10)
+
+        if dxl.open_port():
+            dxl.enable_dynamixel_torque()
+
+            while True:
+                print("Press any key to continue! (or press ESC to quit!)")
+                if getch() == chr(0x1b):
+                    break
+
+                dxl_goal_position = 4000
+                dxl.move_position(dxl_goal_position)
+
+                while True:
+                    dxl_present_position = dxl.read_position()
+                    if not (abs(dxl_goal_position - dxl_present_position) > dxl.dxl_threshold):
+                        break
+
+                dxl.torque_disable()
+                dxl.close_dxl_port()
---
+++ dynamixel.py
@@ -0,0 +1,111 @@
+from dynamixel_sdk import *
+import os
+import serial
+
+
+if os.name == 'nt':
+    import msvcrt
+
+    def getch():
+        return msvcrt.getch().decode()
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class Dynamixel:
+    def __init__(self, addresses, protocol, dxl_id, device_name, min_pos, max_pos, threshold, bd=57600):
+        # Control table address
+        self.address_mx_torque_enable = addresses[0]  # 64, Control table address is different in Dynamixel model
+        self.address_mx_goal_position = addresses[1]  # 116
+        self.address_mx_pos = addresses[2]  # 132
+
+        # Protocol version
+        self.protocol_ver = protocol  # 1.0, See which protocol version is used in the Dynamixel
+
+        # Default setting
+        self.dxl_id = dxl_id  # 1, Dynamixel ID
+        self.baud_rate = bd  # Dynamixel default baudrate : 57600
+        self.device_name = device_name  # Check which port is being used on your controller
+        # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+
+        self.torque_enable = 1  # Value for enabling the torque
+        self.torque_disable = 0  # Value for disabling the torque
+        self.dxl_min_pos = min_pos  # 100, Dynamixel will rotate between this value
+        self.dxl_max_pos = max_pos  # 4000, and this value
+        self.dxl_threshold = threshold  # 20, Dynamixel moving status threshold
+
+        self.portHandler = PortHandler(self.device_name)  # get methods and members of Linux or Windows ports
+
+        self.packetHandler = PacketHandler(self.protocol_ver)  # Protocol1PacketHandler or Protocol2PacketHandler
+
+    def open_port(self):
+        try:
+            self.portHandler.openPort()
+        except serial.SerialException:
+            print("Failed to open the port")
+            return False
+
+        # Set port baud_rate
+        if self.portHandler.setBaudRate(self.baud_rate):
+            print("Succeeded to change the baudrate")
+            return True
+        else:
+            print("Failed to change the baudrate")
+            return False
+
+    def move_position(self, dxl_goal_position):
+        """Write Dynamixel goal position"""
+        dxl_comm_result, dxl_error = self.packetHandler.write4ByteTxRx(self.portHandler, self.dxl_id,
+                                                                       self.address_mx_goal_position, dxl_goal_position)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+
+    def read_position(self):
+        """Read Dynamixel present position"""
+        dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read4ByteTxRx(self.portHandler,
+                                                                                             self.dxl_id,
+                                                                                             self.address_mx_pos)
+        if dxl_comm_result != COMM_SUCCESS:
+            print(self.packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print(self.packetHandler.getRxPacketError(dxl_error))
+
+        print("[ID:%03d] PresPos:%03d\n" % (self.dxl_id, dxl_present_position))
+        return dxl_present_position
+
+    def enable_dynamixel_torque(self):
+        """Enable Dynamixel Torque"""
+        dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, self.dxl_id,
+                                                                       self.address_mx_torque_enable,
+                                                                       self.torque_enable)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+        else:
+            print("Dynamixel#%d has been successfully connected" % self.dxl_id)
+
+    def disable_dynamixel_torque(self):
+        # Disable Dynamixel#1 Torque
+        dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, self.dxl_id,
+                                                                       self.address_mx_torque_enable,
+                                                                       self.torque_disable)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+
+    def close_dxl_port(self):
+        self.portHandler.closePort()
