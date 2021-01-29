5c0c9dca52fb51bf24cefcbc9d63364a0bc4158d
Alex <erashov.ru@mail.ru>
2021-01-29 09:30:21 +0300

    v_2

--- dynamixel.py
+++ dynamixel.py
@@ -66,11 +66,12 @@ class Dynamixel:
         print("[ID:%03d] PresPos:%03d\n" % (self.dxl_id, dxl_present_position))
         return dxl_present_position
 
+    def _send_one_byte(self, address, byte):
+        return self.packetHandler.write1ByteTxRx(self.portHandler, self.dxl_id, address, byte)
+
     def enable_dynamixel_torque(self):
         """Enable Dynamixel Torque"""
-        dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, self.dxl_id,
-                                                                       self.address_mx_torque_enable,
-                                                                       self.torque_enable)
+        dxl_comm_result, dxl_error = self._send_one_byte(self.address_mx_torque_enable, self.torque_enable)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
@@ -79,10 +80,8 @@ class Dynamixel:
             print("Dynamixel#%d has been successfully connected" % self.dxl_id)
 
     def disable_dynamixel_torque(self):
-        # Disable Dynamixel#1 Torque
-        dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, self.dxl_id,
-                                                                       self.address_mx_torque_enable,
-                                                                       self.torque_disable)
+        """Disable Dynamixel Torque"""
+        dxl_comm_result, dxl_error = self._send_one_byte(self.address_mx_torque_enable, self.torque_disable)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
@@ -90,3 +89,13 @@ class Dynamixel:
 
     def close_dxl_port(self):
         self.portHandler.closePort()
+
+    def change_id(self, id):
+        """Change Dynamixel id"""
+        dxl_comm_result, dxl_error = self._send_one_byte(3, id)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+        else:
+            print("Dynamixel#%d has been change id to %d" % self.dxl_id % id)
